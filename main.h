643bc7e08af83d45921483d7c8afae4a834632e0
Noam72T <9823@holbertonstudents.com>
2025-01-08 09:21:40 +0100

    Function & Prototype Corrigé

--- main.h
+++ main.h
@@ -1,12 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <sys/wait.h>
-#include <errno.h>
+#include <unistd.h>
+
+
+extern char **environ;
+
+void prompt(void);
+ssize_t read_input(char **line, size_t *len);
+void execute_command(char *line);
 
 #endif
-
