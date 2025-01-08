d319f1894c7cbdea3dddd55495f1cc61d9b2a7bb
Noam72T <9823@holbertonstudents.com>
2025-01-08 09:19:12 +0100

    Simple Shell 0.1 fix betty

--- main.c
+++ main.c
@@ -14,25 +14,25 @@ void execute_command(char *line);
  */
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
 
-    while (1)
-    {
-        prompt();
-        read = read_input(&line, &len);
-        if (read == -1)
-        {
-            printf("\n");
-            break;
-        }
-        if (strlen(line) == 0)
-            continue;
-        execute_command(line);
-    }
-    free(line);
-    return (0);
+	while (1)
+	{
+		prompt();
+		read = read_input(&line, &len);
+		if (read == -1)
+		{
+			printf("\n");
+			break;
+		}
+		if (strlen(line) == 0)
+			continue;
+		execute_command(line);
+	}
+	free(line);
+	return (0);
 }
 
 /**
@@ -40,7 +40,7 @@ int main(void)
  */
 void prompt(void)
 {
-    printf("#cisfun$ ");
+	printf("#cisfun$ ");
 }
 
 /**
@@ -51,7 +51,7 @@ void prompt(void)
  */
 ssize_t read_input(char **line, size_t *len)
 {
-    return getline(line, len, stdin);
+	return (getline(line, len, stdin));
 }
 
 /**
@@ -60,30 +60,30 @@ ssize_t read_input(char **line, size_t *len)
  */
 void execute_command(char *line)
 {
-    pid_t pid;
-    int status;
-    char *argv[2];
+	pid_t pid;
+	int status;
+	char *argv[2];
 
-    line[strcspn(line, "\n")] = '\0';
-    argv[0] = line;
-    argv[1] = NULL;
+	line[strcspn(line, "\n")] = '\0';
+	argv[0] = line;
+	argv[1] = NULL;
 
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        return;
-    }
-    if (pid == 0)
-    {
-        if (execve(argv[0], argv, environ) == -1)
-        {
-            perror(argv[0]);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-    }
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return;
+	}
+	if (pid == 0)
+	{
+		if (execve(argv[0], argv, environ) == -1)
+		{
+			perror(argv[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+	}
 }
