bc7bdf29268cf59ff921ed67568b068cde71903b
Noam72T <9823@holbertonstudents.com>
2025-01-09 09:49:43 +0100

    Function & Prototype

--- main.h
+++ main.h
@@ -1,13 +1,19 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef SHELL_H
+#define SHELL_H
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
-extern char **environ;
-ssize_t get_input(char **line);
+#define MAX_TOKENS 64
 
-#endif 
+void execute_command(char *command, char **env);
+char **tokenize_input(char *input);
+void free_args(char **args);
+char *get_command_path(char *command, char **env);
+
+#endif
