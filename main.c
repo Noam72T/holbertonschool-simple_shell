0a75ff6a3464321d56c822417e7508f1bcc387ba
Noam72T <9823@holbertonstudents.com>
2025-01-09 09:52:08 +0100

    Entry function of the shell

--- main.c
+++ main.c
@@ -1,88 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
 #include "main.h"
 
-
-void prompt(void)
+/**
+ * main - Entry point for the simple shell program
+ * @ac: Argument count
+ * @av: Argument vector
+ * @env: Environment variables
+ *
+ * Return: 0 on success
+ */
+int main(int ac, char **av, char **env)
 {
-    printf("#cisfun$ ");
-    fflush(stdout);
-}
+    char *input = NULL;
+    size_t input_size = 0;
+    ssize_t read_size;
 
-ssize_t get_input(char **line)
-{
-    size_t len = 0;
-    return getline(line, &len, stdin);
-}
-
-void execute_command(char *line)
-{
-    char *argv[100];
-    pid_t pid = fork();
-
-    if (pid == 0)
-    {
-        char *token = strtok(line, " ");
-        int i = 0;
-
-        while (token != NULL)
-        {
-            argv[i++] = token;
-            token = strtok(NULL, " ");
-        }
-
-        argv[i] = NULL;
-
-        if (execve(argv[0], argv, environ) == -1)
-        {
-            perror("./simple_shell");
-            exit(1);
-        }
-    }
-    else if (pid < 0)
-    {
-        perror("fork");
-        exit(1);
-    }
-    else
-    {
-        wait(NULL);
-    }
-}
-
-int main(void)
-{
-    char *line = NULL;
-    ssize_t read;
+    (void)ac;
+    (void)av;
 
     while (1)
     {
-        prompt();
-        read = get_input(&line);
+        if (isatty(STDIN_FILENO))
+            write(STDOUT_FILENO, "$ ", 2);
 
-        if (read == -1)
+        read_size = getline(&input, &input_size, stdin);
+        if (read_size == -1)
+            break;
+
+        if (input[read_size - 1] == '\n')
+            input[read_size - 1] = '\0';
+
+        if (strcmp("exit", input) == 0)
         {
-            if (feof(stdin))
-            {
-                break;
-            }
-            perror("getline");
-            continue;
+            free(input);
+            exit(0);
         }
 
-        line[strcspn(line, "\n")] = '\0';
-
-        if (strlen(line) == 0)
-        {
-            continue;
-        }
-
-        execute_command(line);
+        execute_command(input, env);
     }
 
-    free(line);
-    return 0;
+    free(input);
+    return (0);
 }
